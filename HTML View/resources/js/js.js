1e5a5b432db5696379cc305569ec69021258ce32
Donovan <vw8111bz@metrostate.edu>
2017-02-27 17:57:01 -0600

    Add files via upload

---
+++ HTML View/resources/js/js.js
@@ -0,0 +1,26 @@
+/*
+Team Drama LLama:
+Carly Coughlin
+KylieRae Daigle
+Joel Hobart
+Patrick Inthisane
+Donovan Smith
+
+Iteration 2
+February 22, 2017
+
+Sources/References:
+Side Navigation Bar: https://www.w3schools.com/howto/howto_js_sidenav.asp
+*/
+
+/* Open Navigation Bar */
+function openNav() {
+    "use strict";
+    document.getElementById("Nav").style.width = "250px";
+}
+
+/* Close Navigation Bar */
+function closeNav() {
+    "use strict";
+    document.getElementById("Nav").style.width = "0px";
+}

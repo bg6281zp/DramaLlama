88d6fccec7c9749064169b801a30b4271fa32660
exual <jhobart@gmail.com>
2017-03-26 21:08:20 -0500

    Initial commit of DB Populate script
    At this point, only has Unit and User tables.

---
+++ Database Scripts/NummyLlama_populate.sql
@@ -0,0 +1,28 @@
+INSERT INTO Unit VALUES('1','ounce');
+INSERT INTO Unit VALUES('2','fluid ounce');
+INSERT INTO Unit VALUES('3','cup');
+INSERT INTO Unit VALUES('4','pint');
+INSERT INTO Unit VALUES('5','quart');
+INSERT INTO Unit VALUES('6','gallon');
+INSERT INTO Unit VALUES('7','teaspoon');
+INSERT INTO Unit VALUES('8','tablespoon');
+INSERT INTO Unit VALUES('9','drop');
+INSERT INTO Unit VALUES('10','unit');
+INSERT INTO Unit VALUES('11','pound');
+INSERT INTO Unit VALUES('12','liter');
+INSERT INTO Unit VALUES('13','milliliter');
+INSERT INTO Unit VALUES('14','gram');
+INSERT INTO Unit VALUES('15','milligram');
+INSERT INTO Unit VALUES('16','kilogram');
+INSERT INTO User VALUES('1','exist','Joel,'Hobart','jhobart@gmail.com','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('2','aardvarkvalcano','Kyli,'Daigle','re9145bv@metrostate.edu','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('3','cc','Carly,'Coughlin','bg6281zp@metrostate.edu','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('4','donovan','Donovan,'Smith','vw8111bz@metrostate.edu','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('5','patrick.inthisane','Patrick,'Inthisane','xz8081pg@metrostate.edu','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('6','goodeaterkenji','Kenji,'Lopez-Alt','kenji@fk_seriouseats.com','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('7','igarontee','Justin,'Wilson','jwilson@fk_justinwilson.com','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('8','jchild','Julia,'Child','jchild@juliachild.com','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('9','kpaul','Paul,'Prudhomme','paul@fk_chefpaul.com','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+INSERT INTO User VALUES('10','beardy','James,'Beard','james@fk_jamesbeard.org','thisisnotapwhash','2017-04-23','2017-04-23','2017-04-23');
+
+ 
